64a35d2cc67766492f93eb5492959ca89800fb8b
itzkingofallheros-hash <itzkingofallheros@gmail.com>
2026-09-12 18:12:22 +0530

    Update print statements in ak.java

--- ak.java
+++ ak.java
@@ -1,7 +1,7 @@
 public class ak
 {
     public static void main(String[] args){
-        System.out.print("hahahahhaha");
-        System.out.print("hello");
+        System.out.print("hi aravind ");
+        System.out.print("\n do you know you are a bleedy womenizer");
     }
 }
